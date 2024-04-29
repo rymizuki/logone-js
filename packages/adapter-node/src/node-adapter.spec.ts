a63794e239f69f75c86e7a3154048b3262398bff
mizuki_r <ry.mizuki@gmail.com>
2024-04-29 23:24:24 +0900

    test: added tests

--- packages/adapter-node/src/node-adapter.spec.ts
+++ packages/adapter-node/src/node-adapter.spec.ts
@@ -1,3 +1,189 @@
-import { describe } from 'vitest'
+import { fakeTimer } from '@logone/test-helper'
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
+import { createAdapter } from './node-adapter'
 
-describe.skip('node-adapter', () => {})
+const timer = fakeTimer()
+
+describe('node-adapter', () => {
+  beforeEach(() => {
+    timer.setup()
+  })
+  afterEach(() => {
+    timer.clear()
+  })
+  describe('debug', () => {
+    it('should be output stdout', () => {
+      const adapter = createAdapter()
+      const spy = vi
+        .spyOn(process.stdout, 'write')
+        .mockImplementation(() => true)
+      adapter.output({
+        type: 'test',
+        config: {
+          elapsedUnit: '1ms'
+        },
+        context: {},
+        runtime: {
+          severity: 'DEBUG',
+          startTime: new Date(),
+          endTime: new Date(),
+          elapsed: 0,
+          lines: [
+            {
+              fileLine: 0,
+              fileName: 'example.ts',
+              message: 'example',
+              payload: [],
+              severity: 'DEBUG',
+              time: new Date()
+            }
+          ]
+        }
+      })
+      expect(spy).toHaveBeenCalled()
+      expect(spy.mock.calls[0]![0]).toEqual(
+        `{"type":"test","config":{"elapsedUnit":"1ms"},"context":{},"runtime":{"severity":"DEBUG","startTime":"${timer.current.toISOString()}","endTime":"${timer.current.toISOString()}","elapsed":0,"lines":[{"fileLine":0,"fileName":"example.ts","message":"example","payload":[],"severity":"DEBUG","time":"${timer.current.toISOString()}"}]}}\n`
+      )
+    })
+  })
+  describe('info', () => {
+    it('should be output stdout', () => {
+      const adapter = createAdapter()
+      const spy = vi
+        .spyOn(process.stdout, 'write')
+        .mockImplementation(() => true)
+      adapter.output({
+        type: 'test',
+        config: {
+          elapsedUnit: '1ms'
+        },
+        context: {},
+        runtime: {
+          severity: 'INFO',
+          startTime: new Date(),
+          endTime: new Date(),
+          elapsed: 0,
+          lines: [
+            {
+              fileLine: 0,
+              fileName: 'example.ts',
+              message: 'example',
+              payload: [],
+              severity: 'INFO',
+              time: new Date()
+            }
+          ]
+        }
+      })
+      expect(spy).toHaveBeenCalled()
+      expect(spy.mock.calls[0]![0]).toEqual(
+        `{"type":"test","config":{"elapsedUnit":"1ms"},"context":{},"runtime":{"severity":"INFO","startTime":"${timer.current.toISOString()}","endTime":"${timer.current.toISOString()}","elapsed":0,"lines":[{"fileLine":0,"fileName":"example.ts","message":"example","payload":[],"severity":"INFO","time":"${timer.current.toISOString()}"}]}}\n`
+      )
+    })
+  })
+  describe('warning', () => {
+    it('should be output stderr', () => {
+      const adapter = createAdapter()
+      const spy = vi
+        .spyOn(process.stderr, 'write')
+        .mockImplementation(() => true)
+      adapter.output({
+        type: 'test',
+        config: {
+          elapsedUnit: '1ms'
+        },
+        context: {},
+        runtime: {
+          severity: 'WARNING',
+          startTime: new Date(),
+          endTime: new Date(),
+          elapsed: 0,
+          lines: [
+            {
+              fileLine: 0,
+              fileName: 'example.ts',
+              message: 'example',
+              payload: [],
+              severity: 'WARNING',
+              time: new Date()
+            }
+          ]
+        }
+      })
+      expect(spy).toHaveBeenCalled()
+      expect(spy.mock.calls[0]![0]).toEqual(
+        `{"type":"test","config":{"elapsedUnit":"1ms"},"context":{},"runtime":{"severity":"WARNING","startTime":"${timer.current.toISOString()}","endTime":"${timer.current.toISOString()}","elapsed":0,"lines":[{"fileLine":0,"fileName":"example.ts","message":"example","payload":[],"severity":"WARNING","time":"${timer.current.toISOString()}"}]}}\n`
+      )
+    })
+  })
+  describe('error', () => {
+    it('should be output stderr', () => {
+      const adapter = createAdapter()
+      const spy = vi
+        .spyOn(process.stderr, 'write')
+        .mockImplementation(() => true)
+      adapter.output({
+        type: 'test',
+        config: {
+          elapsedUnit: '1ms'
+        },
+        context: {},
+        runtime: {
+          severity: 'ERROR',
+          startTime: new Date(),
+          endTime: new Date(),
+          elapsed: 0,
+          lines: [
+            {
+              fileLine: 0,
+              fileName: 'example.ts',
+              message: 'example',
+              payload: [],
+              severity: 'ERROR',
+              time: new Date()
+            }
+          ]
+        }
+      })
+      expect(spy).toHaveBeenCalled()
+      expect(spy.mock.calls[0]![0]).toEqual(
+        `{"type":"test","config":{"elapsedUnit":"1ms"},"context":{},"runtime":{"severity":"ERROR","startTime":"${timer.current.toISOString()}","endTime":"${timer.current.toISOString()}","elapsed":0,"lines":[{"fileLine":0,"fileName":"example.ts","message":"example","payload":[],"severity":"ERROR","time":"${timer.current.toISOString()}"}]}}\n`
+      )
+    })
+  })
+  describe('critical', () => {
+    it('should be output stderr', () => {
+      const adapter = createAdapter()
+      const spy = vi
+        .spyOn(process.stderr, 'write')
+        .mockImplementation(() => true)
+      adapter.output({
+        type: 'test',
+        config: {
+          elapsedUnit: '1ms'
+        },
+        context: {},
+        runtime: {
+          severity: 'CRITICAL',
+          startTime: new Date(),
+          endTime: new Date(),
+          elapsed: 0,
+          lines: [
+            {
+              fileLine: 0,
+              fileName: 'example.ts',
+              message: 'example',
+              payload: [],
+              severity: 'CRITICAL',
+              time: new Date()
+            }
+          ]
+        }
+      })
+      expect(spy).toHaveBeenCalled()
+      expect(spy.mock.calls[0]![0]).toEqual(
+        `{"type":"test","config":{"elapsedUnit":"1ms"},"context":{},"runtime":{"severity":"CRITICAL","startTime":"${timer.current.toISOString()}","endTime":"${timer.current.toISOString()}","elapsed":0,"lines":[{"fileLine":0,"fileName":"example.ts","message":"example","payload":[],"severity":"CRITICAL","time":"${timer.current.toISOString()}"}]}}\n`
+      )
+    })
+  })
+})
--- packages/core/src/logger.spec.ts
+++ packages/core/src/logger.spec.ts
@@ -1,3 +1,207 @@
-import { describe } from 'vitest'
+import { vars } from '@logone/test-helper'
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
+import { LoggerInterface } from './interface'
+import { Logger } from './logger'
+import { Stacker } from './stacker'
+import { Timer } from './timer'
 
-describe.skip('Logger', () => {})
+const v = vars<{
+  logger: LoggerInterface
+  stacker: Stacker
+  timer: Timer
+}>()
+
+describe('Logger', () => {
+  beforeEach(() => {
+    const timer = new Timer({
+      elapsedUnit: '1ms'
+    })
+    const stacker = new Stacker()
+    v.set('timer', timer)
+    v.set('stacker', stacker)
+    v.set('logger', new Logger(timer, stacker))
+    vi.setSystemTime(new Date())
+    vi.isFakeTimers()
+  })
+  afterEach(() => {
+    v.clear()
+  })
+
+  describe('.debug', () => {
+    describe('message only', () => {
+      it('should be stack record', () => {
+        v.get('logger')?.debug('hello')
+        expect(v.get('stacker')?.entries).toStrictEqual([
+          {
+            fileLine: 33,
+            fileName: __filename,
+            message: 'hello',
+            payload: [],
+            severity: 'DEBUG',
+            time: v.get('timer')?.currentTime
+          }
+        ])
+      })
+    })
+    describe('message with payload', () => {
+      it('should be stack record', () => {
+        v.get('logger')?.debug('hello', 1, { message: 'hello' })
+        expect(v.get('stacker')?.entries).toStrictEqual([
+          {
+            fileLine: 48,
+            fileName: __filename,
+            message: 'hello',
+            payload: [1, { message: 'hello' }],
+            severity: 'DEBUG',
+            time: v.get('timer')?.currentTime
+          }
+        ])
+      })
+    })
+  })
+  describe('.info', () => {
+    describe('message only', () => {
+      it('should be stack record', () => {
+        v.get('logger')?.info('hello')
+        expect(v.get('stacker')?.entries).toStrictEqual([
+          {
+            fileLine: 65,
+            fileName: __filename,
+            message: 'hello',
+            payload: [],
+            severity: 'INFO',
+            time: v.get('timer')?.currentTime
+          }
+        ])
+      })
+    })
+    describe('message with payload', () => {
+      it('should be stack record', () => {
+        v.get('logger')?.info('hello', 1, { message: 'hello' })
+        expect(v.get('stacker')?.entries).toStrictEqual([
+          {
+            fileLine: 80,
+            fileName: __filename,
+            message: 'hello',
+            payload: [1, { message: 'hello' }],
+            severity: 'INFO',
+            time: v.get('timer')?.currentTime
+          }
+        ])
+      })
+    })
+  })
+  describe('.warning', () => {
+    describe('message only', () => {
+      it('should be stack record', () => {
+        v.get('logger')?.warning('hello')
+        expect(v.get('stacker')?.entries).toStrictEqual([
+          {
+            fileLine: 97,
+            fileName: __filename,
+            message: 'hello',
+            payload: [],
+            severity: 'WARNING',
+            time: v.get('timer')?.currentTime
+          }
+        ])
+      })
+    })
+    describe('message with payload', () => {
+      it('should be stack record', () => {
+        v.get('logger')?.warning('hello', 1, { message: 'hello' })
+        expect(v.get('stacker')?.entries).toStrictEqual([
+          {
+            fileLine: 112,
+            fileName: __filename,
+            message: 'hello',
+            payload: [1, { message: 'hello' }],
+            severity: 'WARNING',
+            time: v.get('timer')?.currentTime
+          }
+        ])
+      })
+    })
+  })
+  describe('.error', () => {
+    describe('message only', () => {
+      it('should be stack record', () => {
+        v.get('logger')?.error('hello')
+        expect(v.get('stacker')?.entries).toStrictEqual([
+          {
+            fileLine: 129,
+            fileName: __filename,
+            message: 'hello',
+            payload: [],
+            severity: 'ERROR',
+            time: v.get('timer')?.currentTime
+          }
+        ])
+      })
+    })
+    describe('message with payload', () => {
+      it('should be stack record', () => {
+        v.get('logger')?.error('hello', 1, { message: 'hello' })
+        expect(v.get('stacker')?.entries).toStrictEqual([
+          {
+            fileLine: 144,
+            fileName: __filename,
+            message: 'hello',
+            payload: [1, { message: 'hello' }],
+            severity: 'ERROR',
+            time: v.get('timer')?.currentTime
+          }
+        ])
+      })
+    })
+  })
+  describe('.critical', () => {
+    describe('message only', () => {
+      it('should be stack record', () => {
+        v.get('logger')?.critical('hello')
+        expect(v.get('stacker')?.entries).toStrictEqual([
+          {
+            fileLine: 161,
+            fileName: __filename,
+            message: 'hello',
+            payload: [],
+            severity: 'CRITICAL',
+            time: v.get('timer')?.currentTime
+          }
+        ])
+      })
+    })
+    describe('message with payload', () => {
+      it('should be stack record', () => {
+        v.get('logger')?.critical('hello', 1, { message: 'hello' })
+        expect(v.get('stacker')?.entries).toStrictEqual([
+          {
+            fileLine: 176,
+            fileName: __filename,
+            message: 'hello',
+            payload: [1, { message: 'hello' }],
+            severity: 'CRITICAL',
+            time: v.get('timer')?.currentTime
+          }
+        ])
+      })
+    })
+  })
+  describe('.record', () => {
+    describe('message only', () => {
+      it('should be stack record', () => {
+        v.get('logger')?.record('DEBUG', 'hello')
+        expect(v.get('stacker')?.entries).toStrictEqual([
+          {
+            fileLine: 193,
+            fileName: __filename,
+            message: 'hello',
+            payload: [],
+            severity: 'DEBUG',
+            time: v.get('timer')?.currentTime
+          }
+        ])
+      })
+    })
+  })
+})
