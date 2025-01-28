7389172405381e203aa2d6c78ee71f31bb17a43d
mizuki_r <ry.mizuki@gmail.com>
2025-01-28 13:57:35 +0900

    feat(node-adapter): logone record separate content length limit

--- packages/adapter-node/src/node-adapter.ts
+++ packages/adapter-node/src/node-adapter.ts
@@ -1,16 +1,62 @@
 import { LoggerAdapter, LoggerRecord } from '@logone/core'
 import { createCircularReplacer } from './create-circular-replacer'
 
+interface NodeAdapterOptions {
+  contentLengthLimit: number
+}
 export class NodeAdapter implements LoggerAdapter {
-  output(record: LoggerRecord) {
-    const severity = record.runtime.severity
-    const content = this.format(record)
+  constructor(private options: Partial<NodeAdapterOptions> = {}) {}
 
+  output(record: LoggerRecord) {
+    const write = (
+      destination: 'stdout' | 'stderr',
+      record: LoggerRecord & Partial<{ chunked: boolean }>
+    ) => {
+      const content = this.format(record)
+
+      console.log('buffer', Buffer.byteLength(content), content.length)
+      if (
+        !this.options.contentLengthLimit ||
+        Buffer.byteLength(content) < this.options.contentLengthLimit
+      ) {
+        this.write(destination, content)
+      } else {
+        const length = record.runtime.lines.length
+        const half = Math.ceil(length / 2)
+
+        if (length <= 1) {
+          console.warn(
+            `[Logone][node] separate lines by ${this.options.contentLengthLimit} bytes, cannot separate any more.`
+          )
+          this.write(destination, content)
+          return
+        }
+
+        const separated = [
+          record.runtime.lines.slice(0, half),
+          record.runtime.lines.slice(half, length)
+        ]
+
+        separated.forEach((lines) => {
+          write(destination, {
+            ...record,
+            chunked: true,
+            runtime: {
+              ...record.runtime,
+              lines
+            }
+          })
+        })
+      }
+    }
+
+    const severity = record.runtime.severity
     const destination =
       severity === 'CRITICAL' || severity === 'ERROR' || severity === 'WARNING'
         ? 'stderr'
         : 'stdout'
-    this.write(destination, content)
+
+    write(destination, record)
   }
 
   private format(record: LoggerRecord) {
@@ -22,6 +68,6 @@ export class NodeAdapter implements LoggerAdapter {
   }
 }
 
-export function createAdapter() {
-  return new NodeAdapter()
+export function createAdapter(options: Partial<NodeAdapterOptions> = {}) {
+  return new NodeAdapter(options)
 }
